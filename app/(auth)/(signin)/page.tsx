d98bf0fc61605f7bb502f11d2b0530f30c4b7105
Advait Lonkar <lonkaradvait@gmail.com>
2025-02-23 14:40:25 +0530

    landing page responsiveness

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -42,11 +42,10 @@ export default async function AuthenticationPage() {
   }
 
   return (
-    <div className="relative flex min-h-screen flex-col md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative flex h-[15vh] w-full flex-col bg-muted text-white md:h-full dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex h-[15vh] w-full items-center justify-center px-4 md:items-start md:justify-start md:p-10">
-          <div className="flex items-center text-lg font-medium md:mt-8 lg:mt-0">
+    <div className="relative flex min-h-screen flex-col lg:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative flex h-full w-full flex-col text-white lg:h-full dark:border-r">
+        <div className="relative z-20 flex h-[15vh] w-full items-center justify-center px-4 pt-8 sm:pt-10 lg:h-auto lg:items-start lg:justify-start lg:p-10">
+          <div className="flex items-center text-lg font-medium lg:mt-8">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -55,54 +54,64 @@ export default async function AuthenticationPage() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="mr-2 h-5 w-5 sm:h-6 sm:w-6"
+              className="mr-2 h-8 w-8 sm:h-10 sm:w-10 lg:h-10 lg:w-10"
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            <span className="text-base sm:text-lg md:text-xl">Tradezi</span>
+            <span className="text-2xl sm:text-3xl lg:text-4xl">Tradezi</span>
           </div>
         </div>
-        <div className="relative z-20 mt-auto hidden px-6 pb-6 sm:px-8 sm:pb-8 lg:block lg:px-10 lg:pb-10">
-          <blockquote className="space-y-2">
-            <p className="text-base sm:text-lg">
-              {/* &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo; */}
-            </p>
-            {/* <footer className="text-sm">Sofia Davis</footer> */}
-          </blockquote>
+        <div className="relative z-20 flex flex-1 flex-col lg:items-start lg:justify-center">
+          <div className="px-6 pb-8 pt-16 sm:pt-20 lg:px-16 lg:py-0">
+            <div className="space-y-4 text-center lg:text-left">
+              <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl lg:text-5xl">
+                Learn to Trade, <br />
+                Risk-Free
+              </h2>
+              <p className="text-base text-zinc-300 sm:text-lg lg:text-2xl">
+                Master real stock market trading with virtual money.
+                <br />
+                Experience live market conditions... <br />
+                ...without risking your capital.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-1 items-center px-4 py-6 sm:px-6 md:px-8 lg:px-12">
-        <div className="mx-auto flex w-full max-w-[350px] flex-col justify-center space-y-6">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-xl font-semibold tracking-tight sm:text-2xl md:text-3xl">
-              Create an account
-            </h1>
-            {/* <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
-            </p> */}
+
+      <div className="flex flex-1 flex-col lg:mt-[140px] lg:flex lg:h-[calc(100vh-140px)]">
+        <div className="w-full px-6 sm:px-8 lg:hidden">
+          <hr className="border-t border-zinc-600" />
+        </div>
+
+        <div className="flex flex-1 items-center px-4 py-12 sm:px-8 sm:py-16 lg:flex lg:items-center lg:justify-center lg:p-12 ">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:max-w-[400px] lg:max-w-[450px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-xl font-semibold tracking-tight sm:text-2xl lg:text-3xl">
+                Create an account
+              </h1>
+            </div>
+            <div className="w-full">
+              <UserAuthForm />
+            </div>
+            <p className="px-2 text-center text-xs text-muted-foreground sm:text-sm lg:text-base">
+              By clicking continue, you agree to our{' '}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 transition-colors hover:text-primary"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 transition-colors hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
           </div>
-          <div className="w-full">
-            <UserAuthForm />
-          </div>
-          <p className="px-2 text-center text-xs text-muted-foreground sm:px-4 sm:text-sm">
-            By clicking continue, you agree to our{' '}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 transition-colors hover:text-primary"
-            >
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 transition-colors hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
-          </p>
         </div>
       </div>
     </div>
