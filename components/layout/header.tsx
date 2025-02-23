ec6efb8e295e9c2ab26f5032b786bf9d6ff7acc9
Advait Lonkar <lonkaradvait@gmail.com>
2025-02-23 15:50:57 +0530

    mobile sidebar behaviour fixed for responsiveness

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,12 +6,12 @@ import { UserNav } from './user-nav';
 export default function Header() {
   return (
     <header className="sticky inset-x-0 top-0 w-full">
-      <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
-        <div className={cn('block lg:!hidden')}>
+      <nav className="flex items-center justify-between px-4 py-2">
+        <div className={cn('block xl:hidden')}>
           <MobileSidebar />
         </div>
-        <div className="flex items-center gap-2">
-          <UserNav />
+        <div className="ml-auto flex items-center gap-2">
+          {/* <UserNav /> */}
           <ThemeToggle />
         </div>
       </nav>
--- components/layout/mobile-sidebar.tsx
+++ components/layout/mobile-sidebar.tsx
@@ -4,6 +4,7 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { navItems } from '@/constants/data';
 import { MenuIcon } from 'lucide-react';
 import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 // import { Playlist } from "../data/playlists";
 
@@ -19,12 +20,29 @@ export function MobileSidebar({ className }: SidebarProps) {
         <SheetTrigger asChild>
           <MenuIcon />
         </SheetTrigger>
-        <SheetContent side="left" className="!px-0">
+        <SheetContent
+          side="left"
+          className={cn('w-[280px] !px-0', 'md:w-[350px]', 'lg:w-[300px]')}
+        >
           <div className="space-y-4 py-4">
             <div className="px-3 py-2">
-              <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-                Overview
-              </h2>
+              <div className="mb-2 flex items-center px-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-6 w-6"
+                >
+                  <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+                </svg>
+                <span className="text-lg font-semibold tracking-tight">
+                  Tradezi
+                </span>
+              </div>
               <div className="space-y-1">
                 <DashboardNav
                   items={navItems}
