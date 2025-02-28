bafe91d0925333cb4e0b08804059e3fba8587cd6
Advait Lonkar <lonkaradvait@gmail.com>
2025-02-28 10:48:26 +0530

    umami analytics working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,28 @@
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/toaster';
 import '@uploadthing/react/styles.css';
-import type { Metadata } from 'next';
+import type { Metadata, ViewportConfig } from 'next';
 import NextTopLoader from 'nextjs-toploader';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { auth } from '@/auth';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: 'Tradezi',
+  description: 'Trade Easy, Risk Free',
+  other: {
+    'umami-analytics': [
+      'https://umami-analytics-personal.vercel.app/script.js',
+      '74a2b786-a212-47bf-a088-f120a6e3a0fc'
+    ]
+  }
+};
+
+export const viewport: ViewportConfig = {
+  width: 'device-width',
+  initialScale: 1
 };
 
 export default async function RootLayout({
@@ -23,13 +33,6 @@ export default async function RootLayout({
   const session = await auth();
   return (
     <html lang="en">
-      <Head>
-        <script
-          defer
-          src="https://umami-analytics-personal.vercel.app/script.js"
-          data-website-id="74a2b786-a212-47bf-a088-f120a6e3a0fc"
-        ></script>
-      </Head>
       <body
         className={`${inter.className} overflow-hidden `}
         suppressHydrationWarning={true}
@@ -39,6 +42,11 @@ export default async function RootLayout({
           <Toaster />
           {children}
         </Providers>
+        <script
+          defer
+          src="https://umami-analytics-personal.vercel.app/script.js"
+          data-website-id="74a2b786-a212-47bf-a088-f120a6e3a0fc"
+        />
       </body>
     </html>
   );
