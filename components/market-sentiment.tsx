b24e45ab9b72ce2f57eb3dbfa5884dd604507752
r-arvind <rarvind1999@gmail.com>
2025-03-15 16:45:35 +0530

    Added market sentiment analysis section

---
+++ components/market-sentiment.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Modal } from '@/components/ui/modal';
+import dynamic from 'next/dynamic';
+import axios from 'axios';
+import { Skeleton } from '@/components/ui/skeleton';
+import { X } from 'lucide-react';
+import { useToast } from '@/components/ui/use-toast';
+import { AlertModal } from '@/components/modal/alert-modal';
+import { ArrowUp, ArrowDown } from 'lucide-react';
+
+const MarketSentiment = ({ data }) => {
+  const prices = data.data;
+  if (prices.length < 2) {
+    return 'Not enough data to determine trend';
+  }
+
+  // Sort by date in ascending order
+  prices.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  const previous = prices[0]; // Oldest data
+  const latest = prices[prices.length - 1]; //latest data
+
+  const previousPrice = previous.price;
+  const latestPrice = latest.price;
+  const absoluteDifference = (latestPrice - previousPrice).toFixed(2);
+  const percentageChange = ((absoluteDifference / previousPrice) * 100).toFixed(
+    2
+  );
+
+  const previousDate = new Date(previous.date);
+  const latestDate = new Date(latest.date);
+  const dayDifference = Math.abs(
+    (latestDate - previousDate) / (1000 * 60 * 60 * 24)
+  );
+
+  const isPositive = absoluteDifference > 0;
+  const isNegative = absoluteDifference < 0;
+
+  return (
+    <div className="m-2 flex flex-nowrap items-center space-x-2 rounded-lg border p-3 shadow-md md:space-x-4 md:p-4 md:text-xl">
+      <span className="md:text-md text-nowrap text-sm">
+        {data.name}: <span className="font-semibold ">{latestPrice}</span>
+      </span>
+      {isPositive && <ArrowUp className="text-green-500 md:h-4 md:w-4" />}
+      {isNegative && <ArrowDown className="text-red-500 md:h-4 md:w-4" />}
+      <span
+        className={
+          isPositive
+            ? 'text-sm text-green-500'
+            : isNegative
+            ? 'text-sm text-red-500'
+            : 'text-sm text-gray-500'
+        }
+      >
+        {absoluteDifference > 0
+          ? `+${absoluteDifference} (${percentageChange}%)`
+          : `${absoluteDifference} (${percentageChange}%)`}
+      </span>
+    </div>
+  );
+};
+
+export default MarketSentiment;
